f2d27349a8588ee1dcab6a9f7aedc21d84f8be5b
Subashini-12 <113159108+Subashini-12@users.noreply.github.com>
2022-11-08 11:35:49 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,45 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+
+
+
+body{
+  background-color:#5b0deb;
+  width: 100%;
+  height: 100%
+}
+h1{
+  color: rgb(233, 22, 22);
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  padding-top: 30px;
+  font-family: 'Cinzel';
+
+}
+.fr{
+
+  background-color: #ffffff;
+   width: 90%;
+   height: 80%;
+   position: absolute;
+ margin-left: 50px;
+ margin-right: 50px;
+ margin-bottom:50px;
+
+   padding: 20px 20px 20px 20px;
+
+
+   border-radius: 20px;
+   box-shadow: 0 20px 40px rgba(38, 33, 61, 0.2);
+
+}
+#f1{
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privac
